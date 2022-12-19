9db91b9f499e600938b1ef5d21c973f56affa8ae
Adam <adamwirski@gmail.com>
2022-12-19 07:58:29 +0100

    deploy mode

--- testing.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-date_time_str = '09:20:20 PM'
-date_time_obj = datetime.datetime.strptime(date_time_str, '%I:%M:%S %p')
-
-print('Date:', date_time_obj.date())
-print('Time:', date_time_obj.time())
-print('Date-time:', date_time_obj)
